1c8674492961c726b50f4eda5a1fced9f4b798e1
hexthat <hexthat@gmail.com>
2025-10-29 20:46:41 -0700

    Delete location_config.py
    move config to main file

--- location_config.py
+++
@@ -1,8 +0,0 @@
-# location_config.py
-
-Latitude = 36.6277
-Longitude = -119.6766
-timezone = -8
-# Optional day offset to shift the local reference date used by the
-# Solunar Zodiac script. Set to 0 for today, +1 for tomorrow, -1 for yesterday.
-day_offset = 0
